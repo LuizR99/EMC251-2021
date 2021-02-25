74c3f02829e908aae910f8a28a78aa57de5b6860
Luiz Rodrigues <luiz.123felip@gmail.com>
2021-02-25 09:21:50 -0300

    Alterações

---
+++ Aula01/prog1/OlaMundo.java
@@ -0,0 +1,10 @@
+class OlaMundo{
+    public static void main(String[] args) {
+        //Isso é um comentário de uma linha
+        System.out.println("Ola mundo!");
+        /*
+        * Comentário de diversas linhas
+        * Continuação de comentário
+         */
+    }
+}
