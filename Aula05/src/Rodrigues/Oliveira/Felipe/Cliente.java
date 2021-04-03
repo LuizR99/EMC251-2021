582712468142b51a1f170c7d907d8c28dd7e7069
Luiz Rodrigues <luiz.123felip@gmail.com>
2021-04-03 11:45:08 -0300

    Aula 5

---
+++ Aula05/src/Rodrigues/Oliveira/Felipe/Cliente.java
@@ -0,0 +1,20 @@
+package Rodrigues.Oliveira.Felipe;
+
+public class Cliente {
+    private String nome;
+    private String sobrenome;
+    private String cpf;
+
+    public Cliente(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "nome='" + nome + '\'' +
+                ", sobrenome='" + sobrenome + '\'' +
+                ", cpf='" + cpf + '\'' +
+                '}';
+    }
+}
---
+++ aula05-teoria/src/Rodrigues/Oliveira/Felipe/Luiz/Main.java
@@ -0,0 +1,9 @@
+package Rodrigues.Oliveira.Felipe.Luiz;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    String cpf = "529.982.247-25";
+        System.out.println("Cpf valido:" + ValidadorCPF.validarCpf(cpf));
+    }
+}
